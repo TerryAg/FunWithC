977f41a17fa5717484f1ad385cd525d4f544fce9
Terry <terry@agapitos.com.au>
2017-07-08 22:34:19 +0200

    printed board

--- ConnectFour/main.c
+++ ConnectFour/main.c
@@ -4,24 +4,32 @@
 #define BOARD_HEIGHT 6
 
 void create_board(char board[BOARD_LENGTH][BOARD_HEIGHT]) {
-	for (int x = 0; x < BOARD_LENGTH; x++) {
-		for (int y = 0; y < BOARD_HEIGHT; y++) {
+	for (int x = 0; x <= BOARD_LENGTH; x++) {
+		for (int y = 0; y <= BOARD_HEIGHT; y++) {
 			board[x][y] = ' ';
 		}
 	}
 }
 
 void print_board(char board[BOARD_LENGTH][BOARD_HEIGHT]) {
-	for (int x = BOARD_HEIGHT-1; x > 0; x--) {
+	for (int x = BOARD_HEIGHT; x >= 0; x--) {
 		for (int y = 0; y < BOARD_LENGTH; y++) {
 			printf("|%c", board[x][y]);
 		}
 		printf("|\n");
 	}
+	for (int y = 1; y <= BOARD_LENGTH; y++) {
+		printf(" %d", y);
+	}
+	printf("\n");
 }
 
+void turn(char board[BOARD_LENGTH][BOARD_HEIGHT]) {
+
+}
 int main(void) {
 	char board[BOARD_LENGTH][BOARD_HEIGHT];
 	create_board(board);
 	print_board(board);
+	
 }
