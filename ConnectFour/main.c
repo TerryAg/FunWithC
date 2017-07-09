661b94351516679d9caad0f8a62499ba9eadefbd
Terry <terry@agapitos.com.au>
2017-07-09 21:10:25 +0100

    added turn() - not complete

--- ConnectFour/main.c
+++ ConnectFour/main.c
@@ -25,8 +25,29 @@ void print_board(char board[BOARD_LENGTH][BOARD_HEIGHT]) {
 }
 
 void turn(char board[BOARD_LENGTH][BOARD_HEIGHT]) {
-
+	char input = '\0';
+	char *end;
+	int choice, num;
+	printf("Player %d: Pick a column. ");
+	while (fgets(input, MAX_LINE, stdin) != '\0') {
+		choice = strtol(input, &end, 10);
+		if (end == input) {
+			printf("That's not a number! ");
+			continue;
+		}
+		num = (int) choice; // Since strtol returns a long.
+		if ((choice < 1) || (choice > BOARD_LENGTH)) {
+			printf("That number is out of range! ");
+			continue;
+		}
+		if (board[BOARD_HEIGHT-1][choice] != ' ') {
+			printf("Column is full! ")
+			continue;
+		}
+		
+	}
 }
+
 int main(void) {
 	char board[BOARD_LENGTH][BOARD_HEIGHT];
 	create_board(board);
