84e146a6662f70c12fde91648214704a4632a448
Terry <terry@agapitos.com.au>
2017-07-10 13:11:09 +0100

    fixed errors

--- ConnectFour/main.c
+++ ConnectFour/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BOARD_LENGTH 7
 #define BOARD_HEIGHT 6
+#define MAX_LINE 128
 
 void create_board(char board[BOARD_LENGTH][BOARD_HEIGHT]) {
 	for (int x = 0; x <= BOARD_LENGTH; x++) {
@@ -31,13 +33,14 @@ int top_of_col(char board[BOARD_LENGTH][BOARD_HEIGHT], int column) {
 			return x;
 		}
 	}
+	return -1; // full col. shouldn't happen
 }
 
 void turn(char board[BOARD_LENGTH][BOARD_HEIGHT], char pattern) {
-	char input = '\0';
+	char input[MAX_LINE];
 	char *end;
 	int choice, num;
-	printf("Player %d: Pick a column. ");
+	printf("Player #: Pick a column. ");
 	while (fgets(input, MAX_LINE, stdin) != '\0') {
 		choice = strtol(input, &end, 10);
 		if (end == input) {
@@ -45,9 +48,10 @@ void turn(char board[BOARD_LENGTH][BOARD_HEIGHT], char pattern) {
 		} else if ((choice < 1) || (choice > BOARD_LENGTH)) {
 			printf("That number is out of range! ");
 		} else if (board[BOARD_HEIGHT-1][choice] != ' ') {
-			printf("Column is full! ")
+			printf("Column is full! ");
 		} else {
-			board[top_of_col(choice-1)][choice-1] = pattern;
+			board[top_of_col(board, choice-1)][choice-1] = pattern;
+			return;
 		}
 
 	}
@@ -59,5 +63,5 @@ int main(void) {
 	print_board(board);
 	turn(board, 'X');
 	print_board(board);
-
+	return 1;
 }
