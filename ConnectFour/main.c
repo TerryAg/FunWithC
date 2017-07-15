f0b0e557414e42d093fdf38f5f6923e49cf17d50
Terry <terry@agapitos.com.au>
2017-07-15 18:33:56 +0100

    now checks solutions horiz + vert

--- ConnectFour/main.c
+++ ConnectFour/main.c
@@ -46,7 +46,71 @@ int top_of_col(char board[BOARD_LENGTH][BOARD_HEIGHT], int column) {
 	return -1; // full col. shouldn't happen
 }
 
-int game_over(char board[BOARD_LENGTH][BOARD_HEIGHT]) {
+int game_over(char board[BOARD_LENGTH][BOARD_HEIGHT], char pattern, int turns_done) {
+	if (turns_done < 7) {
+		return 0;
+	}
+	int count = 0;
+	int seen;
+	// Check horizontal
+	for (int x = 0; x < BOARD_LENGTH; x++) {
+		seen = 0;
+		for (int y = 0; y < BOARD_HEIGHT; y++) {
+			if (board[y][x] != ' ') {
+				seen++;
+			}
+			if (board[y][x] == pattern) {
+				count++;
+				if (count == 4) {
+					printf("Player won!\n");
+					return 1;
+				}
+			} else {
+				count = 0;
+				if (y > BOARD_LENGTH-4) {
+					// then there's no chance of getting a c4
+					continue;
+				}
+			}
+		}
+		if (seen == 0) {
+			//the row is empty of anything
+			// stop checking horiz rows
+			break;
+		}
+	}
+
+	for (int x = 0; x < BOARD_LENGTH; x++) {
+		seen = 0;
+		for (int y = 0; y < BOARD_HEIGHT; y++) {
+			if (board[x][y] != ' ') {
+				seen++;
+			}
+			if (board[x][y] == pattern) {
+				count++;
+				if (count == 4) {
+					printf("Player won!\n");
+					return 1;
+				}
+			} else {
+				count = 0;
+				if (y > BOARD_HEIGHT-4) {
+					// then there's no chance of getting a c4
+					continue;
+				}
+			}
+		}
+		if (seen == 0) {
+			//the row is empty of anything
+			// stop checking horiz rows
+			break;
+		}
+	}
+
+	if (turns_done == BOARD_HEIGHT*BOARD_LENGTH) {
+		printf("Draw!\n");
+		return 1;
+	}
 	return 0;
 }
 
@@ -74,6 +138,7 @@ int main(void) {
 	char board[BOARD_LENGTH][BOARD_HEIGHT];
 	char pattern = '\0';
 	int curr_player = 1;
+	int turns_done = 0;
 
 	create_board(board);
 	print_board(board);
@@ -94,8 +159,9 @@ int main(void) {
 	while (1) {
 		printf("Player %d, pick a column: ", curr_player);
 		turn(board, pattern);
+		turns_done++;
 		print_board(board);
-		if (game_over(board)) {
+		if (game_over(board, pattern, turns_done)) {
 			break;
 		}
 		pattern = pattern == 'O' ? 'X' : 'O';
